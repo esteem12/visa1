7da7e516e311bd6421c8b65f3816f2d9fd2eb43f
Esther Ibadin-Ehizojie <esther.eibadin@gmail.com>
2023-02-27 22:03:06 +0000

    Version 2

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Deploying a python web application
-echo "Hello World"  #shell scripts    
-print('Hello World') # python script  
+#echo "Hello World"  #shell scripts    
+print('Hello World') # python script
+# I love python language  
 
